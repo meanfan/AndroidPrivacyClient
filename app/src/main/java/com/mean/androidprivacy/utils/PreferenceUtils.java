f9b82b6cbcf6a99a44e0ea68ed6971d0a68337cd
MeanFan <meanfan@outlook.com>
2020-06-04 15:11:32 +0800

    change preference

--- app/src/main/java/com/mean/androidprivacy/utils/PreferenceUtils.java
+++ app/src/main/java/com/mean/androidprivacy/utils/PreferenceUtils.java
@@ -10,7 +10,8 @@ import com.mean.androidprivacy.App;
  */
 public class PreferenceUtils {
     public static final String PACKAGE_NAME = App.class.getName();
-    public static final String AUTHORY = "com.mean.androidprivacy.pref.module";
+    //public static final String AUTHORY = "com.mean.androidprivacy.pref.module";
     public static final String MODULE_CONFIG_NAME = "module_config";
     public static final String ENABLE = "enable";
+    public static final String NOROOT_MODE = "noroot_mode";
 }
